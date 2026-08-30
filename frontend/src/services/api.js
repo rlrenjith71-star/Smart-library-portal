0792811bb4d464c683324dd259b7438a54370205
rlrenjith71-star <rlrenjith71@gmail.com>
2026-08-30 17:27:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/.netlify/functions"
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "/.netlify/functions",
+
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization =
+      `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default api;
